74cede709936950e81a89a2d00a8e5e23dea115f
Frank40q094 <71658275+Frank40q094@users.noreply.github.com>
2020-11-04 09:19:18 -0800

    Enemy AI
    i created a enemy AI Script

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,25 @@
+﻿/////////////////////////////
+///By: Frank Vanris
+///Date: 11/4/2020
+///Description: This will be the enemy AI where if the enemy collides with a wall it changes direction.
+////////////////////////////
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
