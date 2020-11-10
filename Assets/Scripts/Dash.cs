456b2b2f5a83df4b9f76d60380446b9ca6fdbf4b
Thomas Allen <61521655+CaffeinatedLemur@users.noreply.github.com>
2020-11-09 18:08:24 -0800

    Movement 2.0

---
+++ Assets/Scripts/Dash.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dash : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    public float speed;
+    public float time;
+    public float startTime;
+
+    public int direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        time = startTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (direction == 0)
+        {
+            //if (Input.GetKeyDown(KeyCode.Space))
+            //{
+                if (Input.GetKeyDown(KeyCode.D))
+                {
+                    direction = 1;
+                }
+                if (Input.GetKeyDown(KeyCode.A))
+                {
+                    direction = 2;
+                }
+                if (Input.GetKeyDown(KeyCode.W))
+                {
+                    direction = 3;
+                }
+
+                if (Input.GetKeyDown(KeyCode.A) && Input.GetKeyDown(KeyCode.W))
+                {
+                    direction = 4;
+                }
+                if (Input.GetKeyDown(KeyCode.D) && Input.GetKeyDown(KeyCode.W))
+                {
+                    direction = 5;
+                }
+           // }
+        }
+        else
+        {
+            if (time <= 0)
+            {
+                direction = 0;
+                time = startTime;
+                rb.velocity = Vector2.zero;
+            }
+            else
+            {
+                time -= Time.deltaTime;
+
+                if (direction == 1)
+                {
+                    rb.velocity = Vector2.right * speed;
+                }
+                if (direction == 2)
+                {
+                    rb.velocity = Vector2.left * speed;
+                }
+                if (direction == 3)
+                {
+                    rb.velocity = Vector2.up * speed;
+                }
+
+                if (direction == 4)
+                {
+                    rb.velocity = (Vector2.up + Vector2.left) * speed;
+                }
+                if (direction == 5)
+                {
+                    rb.velocity = (Vector2.up + Vector2.right) * speed;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/OtherMovementTest.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+
+public class OtherMovementTest : MonoBehaviour
+{
+	public float m_JumpForce = 400f;                        
+	public float m_MovementSmoothing = .05f;  
+
+
+	private bool canJump;            // Whether or not the player is on the ground.
+	public Rigidbody2D myRb;
+	private bool m_FacingRight = true;  // For determining which way the player is currently facing.
+	private Vector3 velocity = Vector3.zero;
+
+
+
+
+	//how fast you go up and down
+	public float jumpVelocity;
+	public float fallMultiplier = 2.5f;
+
+	//get rigidbody of player
+    public void Start()
+    {
+		myRb = GetComponent<Rigidbody2D>();
+	}
+
+
+
+    public void Move(float move, bool jump)
+	{
+		// Move the character by finding the target velocity
+		Vector3 targetVelocity = new Vector2(move * 10f, myRb.velocity.y);
+		// And then smoothing it out and applying it to the character
+		myRb.velocity = Vector3.SmoothDamp(myRb.velocity, targetVelocity, ref velocity, m_MovementSmoothing);
+
+		if (move > 0 && !m_FacingRight)
+		{
+			Flip();
+		}
+		else if (move < 0 && m_FacingRight)
+		{
+			Flip();
+		}
+		
+		// If the player should jump...
+		if (canJump && jump)
+		{
+			canJump = false;
+			myRb.velocity = Vector2.up * jumpVelocity;
+
+			
+		}
+
+		if (myRb.velocity.y < 0)
+		{
+			myRb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+		}
+	}
+
+
+	private void Flip()
+	{
+		// Switch the way the player is labelled as facing.
+		m_FacingRight = !m_FacingRight;
+
+		// Multiply the player's x local scale by -1.
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+
+	public void OnCollisionStay2D(Collision2D collision)
+	{
+		if (collision.gameObject.tag == "canJump")
+		{
+			//reset boolean
+			canJump = true;
+			
+			//HasJumped = false;
+		}
+
+	}
+}
